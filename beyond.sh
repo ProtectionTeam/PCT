c19d727d8462acd27a7118dffc36f4132785192c
Lucas <EHSAN_SAGHAEE@YAHOO.COM>
2017-03-18 04:35:42 +0330

    Update beyond.sh

--- beyond.sh
+++ beyond.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $HOME/BDReborn
+cd $HOME/PCT
 
 install() {
 	    cd tg
@@ -67,7 +67,7 @@ if [ ! -f ./tg/tgcli ]; then
    echo -e "     >>>>                       We Are Not Alliance                             "
    echo -e "     >>>>                       We Are Family                                   "
    echo -e "     >>>>                       We Are The Best :-)                             "
-   echo -e "     >>>>                       @BeyondTeam                                     "
+   echo -e "     >>>>                       @ProtectionTeam                                     "
    echo -e "\033[0;00m"
    echo -e "\e[36m"
    logo_play
