5077293c63966ae88490638d56f885008de7b800
Lucas <EHSAN_SAGHAEE@YAHOO.COM>
2017-03-18 04:39:44 +0330

    ProtectionTeam

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COUNTER=1
 while(true) do
-./beyond.sh
+./pct.sh
 curl "https://api.telegram.org/botتوکن/sendmessage" -F "chat_id=ایدی خودتون" -F "text=#NEWCRASH-#TeleBeyond-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
