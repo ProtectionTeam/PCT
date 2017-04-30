6ea2592dfe571df56f211181251f04cf32876aca
Lucas <EHSAN_SAGHAEE@YAHOO.COM>
2017-04-30 09:55:15 -0700

    Update del.lua

--- plugins/del.lua
+++ plugins/del.lua
@@ -1,4 +1,4 @@
-﻿local function delmsg (i,naji)
+local function delmsg (i,naji)
     msgs = i.msgs 
     for k,v in pairs(naji.messages_) do
         msgs = msgs - 1
@@ -11,13 +11,13 @@
     tdcli.getChatHistory(naji.messages_[0].chat_id_, naji.messages_[0].id_,0 , 100, delmsg, {msgs=msgs})
 end
 local function run(msg, matches)
-    if matches[1] == 'del' and is_owner(msg) then
-        if tostring(msg.to.id):match("^-100") then 
+    if matches[1] == 'del' or matches[1] == 'حذف' and is_owner(msg) then
+        if tostring(msg.to.id):match("^-100") then
             if tonumber(matches[2]) > 1000 or tonumber(matches[2]) < 1 then
                 return  '🚫 *1000*> _تعداد پیام های قابل پاک سازی در هر دفعه_ >*1* 🚫'
             else
-				tdcli.getChatHistory(msg.to.id, msg.id,0 , 100, delmsg, {msgs=matches[2]})
-				return "`"..matches[2].." `_پیام اخیر با موفقیت پاکسازی شدند_ 🚮"
+        tdcli.getChatHistory(msg.to.id, msg.id,0 , 100, delmsg, {msgs=matches[2]})
+        return ""..matches[2].." _پیام اخیر با موفقیت پاکسازی شدند_ 🚮"
             end
         else
             return '⚠️ _این قابلیت فقط در سوپرگروه ممکن است_ ⚠️'
@@ -26,11 +26,11 @@ local function run(msg, matches)
 end
 return {
     patterns = {
-        '^[!#/]([Dd][Ee][Ll]) (%d*)$',
+        command .. "([Dd][Ee][Ll]) (%d+)$",
+        '^([Dd][Ee][Ll]) (%d+)$',
     },
+            patterns_fa = {
+'^(حذف) (%d+)$',
+},
     run = run
 }
-
---Write By @OFFLlNE
---Open By @LuaError
---Channel @LuaError
