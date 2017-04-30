c217af0149d1df85b3a968b17cf8839a4c30976f
Lucas <EHSAN_SAGHAEE@YAHOO.COM>
2017-04-30 09:47:09 -0700

    Update utils.lua

--- bot/utils.lua
+++ bot/utils.lua
@@ -71,7 +71,7 @@ end
 --  Saves file to /tmp/. If file_name isn't provided,
 -- will get the text after the last "/" for filename
 -- do ski
-BDRpm = '\n@BeyondTeam'
+BDRpm = '\n@ProtectionTeam'
 -- Waiting for ski:)
 -- and content-type for extension
 function download_to_file(url, file_name)
