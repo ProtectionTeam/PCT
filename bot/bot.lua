113ffe28a4076bf60e8d771521fd2310ce77531c
Lucas <EHSAN_SAGHAEE@YAHOO.COM>
2017-05-23 13:53:18 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -75,7 +75,7 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
 	config = {
     enabled_plugins = {
-		"PCT",
+		"Lucas",
 		"msg-checks",
 		"tools"
 	},
